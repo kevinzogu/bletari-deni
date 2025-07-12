39747d9529e96c9211c8f15db1bb20d30c9c1329
kevinzogu <kevinzogu@gmail.com>
2025-07-12 21:08:44 +0200

    Add PWA support with manifest and service worker

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'bletari-deni-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    // Add other assets (CSS, JS, images) as needed
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames.map(name => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                })
+            )
+        )
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
